cc9c65644eee0820fd823cfb113880d91668010a
akolbin <kolbin.alex@gmail.com>
2022-11-04 10:00:55 -0400

    simple fake login page

--- src/App.js
+++ src/App.js
@@ -2,27 +2,32 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import './App.css';
+import './Login.css';
 
+import Login from './pages/Login';
 import Home from './pages/Home';
 import SingleRoom from './pages/SingleRoom';
 import Rooms from './pages/Rooms';
 import Contacts from './pages/Contacts';
 import About from './pages/About';
-import Error from './pages/Error';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/rooms" component={Rooms} />
-        <Route exact path="/rooms/:slug" component={SingleRoom} />
-        <Route exact path="/Contacts" component={Contacts} />
-        <Route exact path="/About" component={About} />
-        <Route exact component={Error} />
-      </Switch>
+      <Route>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <div>
+        <Navbar />
+          <Route exact path="/Home" component={Home} />
+          <Route exact path="/rooms" component={Rooms} />
+          <Route exact path="/rooms/:slug" component={SingleRoom} />
+          <Route exact path="/Contacts" component={Contacts} />
+          <Route exact path="/About" component={About} />
+          </div>
+        </Switch>
+      </Route>
     </>
   );
 }
